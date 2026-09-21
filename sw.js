ce692a452518b9f6f14c9451b04ebc5f06a06e55
Smldav <zkj2pt69h7@privaterelay.appleid.com>
2026-09-21 13:27:05 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE='muscu-v1';
+const CACHE='muscu-v3';
 const FILES=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
+  e.waitUntil(caches.open(CACHE).then(c=>Promise.all(FILES.map(f=>c.add(new Request(f,{cache:'reload'}))))));
   self.skipWaiting();
 });
 self.addEventListener('activate',e=>{
@@ -10,11 +10,14 @@ self.addEventListener('activate',e=>{
 });
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET') return;
+  if(new URL(e.request.url).origin!==self.location.origin) return; // images et polices externes : gérées par le navigateur
   e.respondWith(
-    caches.match(e.request).then(hit=>hit||fetch(e.request).then(res=>{
-      const copy=res.clone();
-      caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});
+    fetch(e.request).then(res=>{
+      if(res&&res.ok){
+        const copy=res.clone();
+        caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});
+      }
       return res;
-    }).catch(()=>caches.match('./index.html')))
+    }).catch(()=>caches.match(e.request).then(h=>h||caches.match('./index.html')))
   );
 });
